5ced8449b95a9c9d963bf206e711b3c8c93cf051
Lis0u <elise.schrav@gmail.com>
2015-09-12 16:02:31 +0200

    VIM: config

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@ git submodule update --init --recursive
 cp bash/bashrc ~/.bashrc
 cp git/gitconfig ~/.gitconfig
 cp valgrind/valgrindrc ~/.valgrindrc
+cp vim/vimrc_perso ~/.vimrc
+mv vim/bundle/YouCompleteMe vim/_YouCompleteMe
