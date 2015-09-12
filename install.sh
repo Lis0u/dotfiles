834ccce045d03d5214b0bf6e21ae5a6a09ec2355
Lis0u <elise.schrav@gmail.com>
2015-09-12 16:15:13 +0200

    VIM

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ cp bash/bashrc ~/.bashrc
 cp git/gitconfig ~/.gitconfig
 cp valgrind/valgrindrc ~/.valgrindrc
 cp vim/vimrc_perso ~/.vimrc
-mv vim/bundle/YouCompleteMe vim/_YouCompleteMe
+#mv vim/bundle/YouCompleteMe vim/_YouCompleteMe
